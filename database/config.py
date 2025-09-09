30aa1be75ee8b4124682c30424b515d5b28da7e9
yzokenm <yzokenm@gmail.com>
2025-09-09 12:40:17 +0500

    ignore token

---
+++ database/config.py
@@ -0,0 +1,23 @@
+BOT_TOKEN = "8408778951:AAHjZeQkH5Ho2mfYJ7mT3mxXk1ZwpPoSkSM"
+
+# MySQL DB Config
+DB_CONFIG = {
+    "host": "127.0.0.1",
+    "port": 3306,
+    "user": "Yzoken",
+    "password": "yzoken_Asdfsfekj13$19",
+    "database": "Yzoken$Chopar",
+    "charset": "utf8mb4",
+    "collation": "utf8mb4_0900_ai_ci"
+}
+
+CITIES = ["Shakhrikhan", "Andijan", "Namangan", "Fergana", "Tashkent"]
+SEAT_OPTIONS = [1, 2, 3, 4, 5]
+PRICE_OPTIONS = [50000, 70000, 100000, 150000]
+
+
+TIME_OPTIONS = [
+    "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00",
+    "12:00", "13:00", "14:00", "15:00", "16:00", "17:00",
+    "18:00", "19:00", "20:00", "21:00", "22:00"
+]
